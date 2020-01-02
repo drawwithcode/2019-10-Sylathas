fc9f2be0b4a391e241a861be839d63e09e68f030
sylathas <abate.niccolo@gmail.com>
2020-01-02 16:31:41 +0100

    Fixes

--- sketch.js
+++ sketch.js
@@ -3,17 +3,15 @@ var maxColorValue = 255;
 var minColorValue = 0;
 
 function preload(){
+    //Preload Cat model and Texture
     cat = loadModel("assets/cat.obj", true);
     cattexture = loadImage("assets/Cat-texture.jpg");
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
-  color(
-      map(mouseY, 0, height, maxColorValue, minColorValue),
-      map(mouseX, 0, width, maxColorValue, minColorValue),
-      map(mouseY, 0, width, minColorValue, maxColorValue)
-  );
+
+  //Create instructions using a div
   help = createDiv('Move the mouse to change colors.<br> Click and drag to orbit.<br> Scroll to zoom.');
   help.style('position: absolute; width: 25vw; height: 10vh; bottom: 2%; left: 50%; transform: translate(-50%); color: white; text-align: center; font-family: Helvetica; font-size: 1vw');
 }
@@ -21,43 +19,29 @@ function setup() {
 function draw() {
   clear();
     
-  var help = text('Move the mouse to change the colors. Click and drag to orbit around the cats. Use the scrolling wheel to zoom.', windowWidth/2, windowHeight - 30);
-    
+  //Let the user move around the 3D Space with the mouse
   orbitControl(1, 1, 0.1);
     
+  //Change background color moving the mouse
   background(
       map(mouseX, 0, width, maxColorValue, minColorValue),
       map(mouseY, 0, height, maxColorValue, minColorValue),
       map(mouseX, 0, height, minColorValue, maxColorValue)
     );
-    
-  directionalLight(
-      map(mouseX, 0, height, minColorValue, maxColorValue),
-      map(mouseY, 0, height, maxColorValue, minColorValue),
-      map(mouseY, 0, height, maxColorValue, minColorValue), -(width / 5 / width - 0.5) * 2, -(2 * height / height - 0.5) * 2, -1);
 
-
-    // Chest color light
-    pointLight(
-
-      map(mouseX, 0, width, maxColorValue, minColorValue),
-      map(mouseX, 0, width, minColorValue, maxColorValue),
-      map(mouseX, 0, height, minColorValue, maxColorValue), 0, -70, 80);
-
-    // Giving Mao the main colors with lights
     // Main light
-
     directionalLight(
       map(mouseX, 0, height, minColorValue, maxColorValue),
       map(mouseY, 0, height, maxColorValue, minColorValue),
-      map(mouseX, 0, width, maxColorValue, minColorValue), 1, 0.5, 0);
-
-    // Secondary light, smoothens shadows
+      map(mouseX, 0, width, maxColorValue, minColorValue), 1, 1, 0);
+    
+    //Secondary Light
     directionalLight(
       map(mouseX, 0, height, minColorValue, maxColorValue),
       map(mouseY, 0, height, maxColorValue, minColorValue),
-      map(mouseX, 0, width, maxColorValue, minColorValue), -1, 0, 0);
+      map(mouseX, 0, width, maxColorValue, minColorValue), -0.5, 0, 0);
 
+  //Create 80 cats and make them move in a sin function
   for (let j = 0; j < 5; j++) {
     push();
     for (let i = 0; i < 80; i++) {
@@ -69,8 +53,10 @@ function draw() {
       rotateZ(frameCount * 0.002);
       push();
       noStroke();
+      //Add texture to cat
       texture(cattexture);
       scale(0.4);
+      //Load cat
       model(cat);
       pop();
     }
